efb14ce4faf44d7c2f8791e2c6663a5adaa86815
dflores2017 <31196113+dflores2017@users.noreply.github.com>
2019-11-07 03:11:25 -0300

    filtrado de proyecto de ley e impresion de votos

--- holy.py
+++ holy.py
@@ -6,99 +6,82 @@ import os.path as path
 import nltk
 from nltk.corpus import stopwords
 from unidecode import unidecode
-##if path.exists('sesion.json'):
-##     infile = open("sesion.json","r")
-##     contents = infile.read()
-##     soup = BeautifulSoup(contents,'xml')
-##     titles = soup.find_all('BOLETINXML')
-##     for title in titles:
-##          print "NUMERO: ", title.SESION['NUMERO']
-##          print titles
-##else:
-#opcion=input("ingrese 1 para obtener datos de legislatura actual y 2 para obtener informacion completa  ")
+import re
+#from pymongo import *
 
-#try:  
-     
-#     if opcion==1:
 connect = urllib2.urlopen("http://opendata.camara.cl/wscamaradiputados.asmx/getLegislaturaActual")
+#connect = urllib2.urlopen('http://opendata.camara.cl/wscamaradiputados.asmx/getLegislaturas')
+
 info= connect.read()
 soupAc = BeautifulSoup(info,'xml')
 idAc = soupAc.find_all('ID')
 idNum = []
 for i in idAc:
-     idNum=(str((i.get_text())))
+     idNum=(str((i.get_text())))        #Id legislatura actual
 
 print idNum
 idSesiones = urllib2.urlopen("http://opendata.camara.cl/wscamaradiputados.asmx/getSesiones?prmLegislaturaID="+idNum)
 soupSec = BeautifulSoup(idSesiones,'xml')
-idSec = soupSec.find_all('ID')     #id legislatura
+idSec = soupSec.find_all('ID')     
 idNumSec = []
 for i in idSec:
-     idNumSec.append(str((i.get_text())))
+     idNumSec.append(str((i.get_text())))    #Id sesiones 
 print idNumSec
 basura = '<?xml version="1.0" encoding="utf-8"?>'
+idProyLey = []
 for i in idNumSec:
-     prueba= idNumSec[0]
-     idBoletin = urllib2.urlopen("http://opendata.camara.cl/wscamaradiputados.asmx/getSesionBoletinXML?prmSesionID="+prueba)
+     idBoletin = urllib2.urlopen("http://opendata.camara.cl/wscamaradiputados.asmx/getSesionBoletinXML?prmSesionID="+i)
      var= idBoletin.read()
      if (var != basura):
-          #sesion = open('sesion.json','w')
           soupBol = BeautifulSoup(var,'xml')
           text = soupBol.get_text(strip=True)
-          text= unidecode(text)
-          tokens = [t for t in text.split()]
-          clean_tokens = tokens[:] 
-          sr = stopwords.words('spanish')
-          for token in tokens:
-              if token in stopwords.words('spanish'):
-                  clean_tokens.remove(token)
-          freq = nltk.FreqDist(clean_tokens)
-          for key,val in freq.items():
-               key= key.encode('utf-8')
-               print (str(key) + ':' + str(val))
-     break
+          text= unidecode(text)         #boletin en ascii sin tags
+          for proy in soupBol.find_all('PROYECTO_LEY'):
+               idProyLey.append(str((proy.get('BOLETIN'))))
+idProyLey2= []
+for i in idProyLey:
+     i = i.replace('y',',')
+     i = i.replace('Y',',')
+     i = i.replace('.','') 
+     cadena = i.replace(" ","")
+     element = cadena.split(',')
+     for j in element:
+          if j!="":
+               idProyLey2.append(str(j))
+          else:
+               pass
+print idProyLey2
+
+idVotacion = []
+for i in idProyLey2:
+     proyecto = urllib2.urlopen("http://opendata.camara.cl/camaradiputados/WServices/WSLegislativo.asmx/retornarProyectoLey?prmNumeroBoletin="+i)
+     soupProy = BeautifulSoup(proyecto,'xml')
+     for vol in soupProy.select('VotacionProyectoLey > Id'):
+          idVotacion.append(str((vol.get_text())))
+
+print idVotacion
+
+for i in idVotacion:
+     votos = urllib2.urlopen("http://opendata.camara.cl/camaradiputados/WServices/WSLegislativo.asmx/retornarVotacionDetalle?prmVotacionId="+i)
+     info = votos.read()
+     print info
+##          tokens = [t for t in text.split()]
+##          clean_tokens = tokens[:] 
+##          sr = stopwords.words('spanish')
+##          for token in tokens:
+##              if token in stopwords.words('spanish'):
+##                  clean_tokens.remove(token)
+##          freq = nltk.FreqDist(clean_tokens)
+##          for key,val in freq.items():
+##               key= key.encode('utf-8')
+##               #print (str(key) + ':' + str(val))
+##          print i
+    # break
+##print "listo"
 
                
 
-          #orDia =  soupBol.find_all('ORDEN_DIA')       #info completa de proyecto ley
-          #for i in orDia:
-           #    print "NUMERO: ", title.SESION['NUMERO']
-            #   print i
-          #sesion.write(var)
-          #sesion.close
-          #print orDia
-          
 
-#     if opcion==2:
-##          connect = urllib2.urlopen('http://opendata.camara.cl/wscamaradiputados.asmx/getLegislaturas')
-##          connect2 =  connect.read()
-##
-##          soup = BeautifulSoup(connect2,'xml')
-##          ids = soup.find_all('ID')     #id legislatura
-##          id1= []                       
-##          for i in ids:
-##               id1.append(str((i.get_text())))
-##          id1l= []
-##          for i in id1:
-##               legislaturasid = urllib2.urlopen("http://opendata.camara.cl/wscamaradiputados.asmx/getSesiones?prmLegislaturaID="+i)
-##               soup = BeautifulSoup(legislaturasid,'xml')
-##               idss = soup.find_all('ID')     #id legislatura
-##               for j in idss:
-##                    id1l.append(str((j.get_text())))
-##          basura = '<?xml version="1.0" encoding="utf-8"?>'
-##          for i in id1l:
-##               sesionesid = urllib2.urlopen("http://opendata.camara.cl/wscamaradiputados.asmx/getSesionBoletinXML?prmSesionID="+i)
-##               var= sesionesid.read()
-##               if (var != basura):
-##                    sesion = open('sesion.json','w')
-##                    sesion.write(var)
-##                    sesion.close
-##                    print var
-#     if opcion!=1 or opcion!=2:
-#          print "opcion invalida"
-
-#except:
-#     print "opcion invalida"
 
 
 
